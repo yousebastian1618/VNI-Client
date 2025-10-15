6a41868006b9efa90f42cef7d945fb0499da0071
you.sebastian.1.618@gmail.com <you.sebastian.1.618@gmail.com>
2025-10-14 22:00:08 -0700

    Added client

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   cross_domain: '',
   environ: 'development',
-  client: 'https://vitali-client.office-ec6.workers.dev',
+  client: 'https://vni-client.pages.dev',
   server_url: 'https://vni-server-153951246739.us-west1.run.app'
 }
